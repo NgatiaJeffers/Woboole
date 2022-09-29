5ef6745d8ca09d02f4e42f808d8eaa57b3b70b69
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-09-29 10:43:54 +0300

    Correctly serving optimization of images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    loader: "akamai",
+    domains: ["images.unsplash.com"],
+  }
 }
+
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,14 @@ const HomePage = () => {
 
 
         <div className={styles.grid}>
+          <div style={{ width: 500, height: 200, position: 'relative', marginTop: '10px' }}>
+            <Image 
+              src={'https://images.unsplash.com/photo-1605460375648-278bcbd579a6'}
+              layout='fill'
+              objectFit='cover'
+              alt='A beautiful English Setter'
+            />
+          </div>
           
         </div>
       </main>
